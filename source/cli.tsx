ebcb466394798c5a20ebc4f80331ae15dcf14e1e
rossamurphy <ross.a.murphy@gmail.com>
2024-05-21 16:07:56 +0100

    3rd time lucky.

--- source/cli.tsx
+++ source/cli.tsx
@@ -1,9 +1,20 @@
 #!/usr/bin/env node
 import Pastel from 'pastel';
 import ora from "ora";
+import {Command} from 'commander';
 
 const app = new Pastel({
 	importMeta: import.meta,
 });
+
+const program = new Command();
+
+const version = '0.0.20';
+
+program.version(version, '-v, --version', 'output the current version').description(
+	"Official CLI for OneContext!"
+)
+
+program.parse(process.argv);
 
 await app.run();
